45bdcbb75ab581aa53377face42141df5e9d2abc
AyoDavid26 <folayanayodeji@gmail.com>
2024-07-11 09:05:55 +0100

    New Updates

--- marketplce-extension/backgground.js
+++ marketplce-extension/backgground.js
@@ -1,1 +1,16 @@
-console.log('Background script loaded.');
+chrome.runtime.onInstalled.addListener(() => {
+    console.log('Price Comparator Extension Installed');
+  });
+  
+  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.action === "fetchPrices") {
+      // Dummy data for fetched prices
+      const fetchedPrices = [
+        { marketplace: 'Amazon', price: '$100' },
+        { marketplace: 'eBay', price: '$90' },
+        { marketplace: 'Alibaba', price: '$80' }
+      ];
+      sendResponse({ status: "success", prices: fetchedPrices });
+    }
+  });
+  
